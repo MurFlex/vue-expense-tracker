549d4c8f3cac282cd0299ce0cb05542f5249981e
Kirill Mahonin <murflexmur@yandex.ru>
2024-02-12 00:40:33 +0300

    Balance component implemented

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
 	<Header />
+	<div class="container">
+		<Balance />
+	</div>
 </template>
 
 <script>
+import Balance from './components/Balance.vue'
 import Header from './components/Header.vue'
 
 export default {
 	components: {
 		Header,
+		Balance,
 	},
 }
 </script>
